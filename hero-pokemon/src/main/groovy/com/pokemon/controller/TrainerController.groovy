ee589e587910c384d5b493d9099c9880766964b3
Hércules S. S. José <hercules_hssj@outlook.com>
2020-04-21 01:15:39 -0300

    Inclusão do import que faltava

--- hero-pokemon/src/main/groovy/com/pokemon/controller/TrainerController.groovy
+++ hero-pokemon/src/main/groovy/com/pokemon/controller/TrainerController.groovy
@@ -7,6 +7,7 @@ import com.pokemon.service.WildPokemonService
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.PathVariable
